549f7a685dbafe9e9660524e73bbd59ec7d4cf93
Batoy_Nicholo <nicholox08@gmail.com>
2026-08-08 08:54:17 +0800

    Create Program.cs

---
+++ ComputerLaboratoryUsageMonitoringSystem/Program.cs
@@ -0,0 +1,29 @@
+using ComputerLaboratoryUsageMonitoringSystem.Repositories;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Account/Login";
+        options.AccessDeniedPath = "/Account/Login";
+    });
+
+builder.Services.AddSingleton<UserRepository>();
+builder.Services.AddSingleton<LaboratorySessionRepository>();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
