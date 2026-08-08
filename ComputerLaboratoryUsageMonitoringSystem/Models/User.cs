da6a9e09efbfc6f29dfd3db102957881c41af0ed
Batoy_Nicholo <nicholox08@gmail.com>
2026-08-08 08:59:31 +0800

    Create User.cs

---
+++ ComputerLaboratoryUsageMonitoringSystem/Models/User.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ComputerLaboratoryUsageMonitoringSystem.Models;
+
+public class User
+{
+    public int Id { get; set; }
+
+    [Required]
+    [Display(Name = "First Name")]
+    public string FirstName { get; set; } = "";
+
+    [Required]
+    [Display(Name = "Last Name")]
+    public string LastName { get; set; } = "";
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = "";
+
+    [Required]
+    public string Username { get; set; } = "";
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = "";
+}
